f521923bc9724ac7da4a9175931c51f58675f2e5
1PauloRod <70543693+1PauloRod@users.noreply.github.com>
2024-07-20 02:21:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from time import sleep
 
 def main() -> None:
     botTwitter = BotTwitter()
-    
-    while True:
-        botTwitter.process_content()
-        sleep(5)
-        
+    botTwitter.process_content()
+    sleep(5)
+
 
 if __name__ == '__main__':
     main()
-    
